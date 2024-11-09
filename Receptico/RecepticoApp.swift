c3045bde333508cb6c7918d2c9acf10ff6101451
KyryloAlekseienko <kirill220874041@gmail.com>
2024-11-09 19:29:59 +0200

    09_11_2024
    Project creation, first launch

---
+++ Receptico/RecepticoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecepticoApp.swift
+//  Receptico
+//
+//  Created by Кирилл Алексеенко on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecepticoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
