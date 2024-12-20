81a5d13d87e4eb8a2a04f7bf8b08dbe8bfd2e317
GZoneDev <vlad.krivenko.over90@gmail.com>
2024-12-20 23:11:05 +0200

    Create Common => Validation: create validation tools

---
+++ lib/common/valitation/template_validate.dart
@@ -0,0 +1,52 @@
+import 'package:receptico/common/valitation/validation_builder.dart';
+
+class TemplateValidate {
+  static const int minPasswordLeng = 6;
+
+  static bool required(String? validateValue) =>
+      validateValue == null || validateValue.isEmpty;
+
+  static ValidationBuilder _email() {
+    return ValidationBuilder()
+        .required('Введіть пошту')
+        .email('Недійсна пошта');
+  }
+
+  static ValidationBuilder _phone() {
+    return ValidationBuilder()
+        .required('Введіть дійсний номер телефону')
+        .phone('Недійсний номер телефону');
+  }
+
+  static ValidationBuilder _password() {
+    return ValidationBuilder()
+        .required('Введіть пароль')
+        .minLength(minPasswordLeng,
+            'Пароль має бути не менше $minPasswordLeng символів')
+        .regExp(
+            RegExp(r'[A-Z]'), 'Пароль має містити принаймні одну велику літеру')
+        .regExp(
+            RegExp(r'[a-z]'), 'Пароль має містити принаймні одну малу літеру')
+        .regExp(RegExp(r'[0-9]'), 'Пароль повинен містити хоча б одне число')
+        .regExp(RegExp(r'[\W_]'),
+            'Пароль повинен містити хоча б один спеціальний символ');
+  }
+
+  static String? emailValidate(String value) {
+    final validateEmail = _email().build();
+    final result = validateEmail(value);
+    return result;
+  }
+
+  static String? passwordValidate(String value) {
+    final validatePassword = _password().build();
+    final result = validatePassword(value);
+    return result;
+  }
+
+  static String? phoneValidate(String value) {
+    final validatePhone = _phone().build();
+    final result = validatePhone(value);
+    return result;
+  }
+}
