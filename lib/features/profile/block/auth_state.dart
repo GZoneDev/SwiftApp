9b7a6ba3a3cb0064fcd00f4d29119a3d04db803b
KyryloAlekseienko <kirill220874041@gmail.com>
2024-12-24 15:47:09 +0200

    add file

---
+++ lib/features/profile/block/auth_state.dart
@@ -0,0 +1,32 @@
+part of 'auth_block.dart';
+
+abstract class AuthState {}
+
+class AuthInitial extends AuthState {}
+
+class AuthLoading extends AuthState {}
+
+class AuthLoaded extends AuthState {}
+
+class AuthLoginSuccess extends AuthState {}
+
+class AuthRegisterSuccess extends AuthState {}
+
+class AuthNotFail extends AuthState {}
+
+class AuthLoginFailure extends AuthState {
+  String? emailError, passwordError;
+
+  AuthLoginFailure({this.emailError, this.passwordError});
+}
+
+class AuthRegisterFailure extends AuthState {
+  String? emailError, passwordError, phoneError, usernameError;
+
+  AuthRegisterFailure({
+    this.emailError,
+    this.passwordError,
+    this.phoneError,
+    this.usernameError,
+  });
+}
